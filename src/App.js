1ee8f4ed0aed9d8898d13567fc4b7b639639e4a1
JoasMartins <joasmcarmo@gmail.com>
2023-10-29 17:31:26 -0300

    page download

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Index from '../pages/index'; // Importe o componente da nova página
-import Download from '../pages/download';
+import Index from './pages/index'; // Importe o componente da nova página
+import Download from './pages/download';
 
 function App() {
   //document.title = "Agenda Patrus - Download"
